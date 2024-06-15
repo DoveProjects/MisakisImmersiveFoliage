0b58c392b5dea3d1d5f4921b9b0b296826c0866c
DoveProjects <stewball000@gmail.com>
2024-06-15 16:30:46 -0700

    tried to fix collision

--- MIF/MIF/MIFModSystem.cs
+++ MIF/MIF/MIFModSystem.cs
@@ -1,13 +1,21 @@
 ﻿using HarmonyLib;
 using Vintagestory.GameContent;
+using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.MathTools;
+using Vintagestory.API.Util;
+using Vintagestory.Essentials;
 
 namespace Vintagestory.GameContent
 {
     [HarmonyPatch(typeof(BlockLeaves))]
     [HarmonyPatch("GetCollisionBoxes")]
-    class BEBehaviorSupportBeam_GetCollisionBoxes_Patch
+    
+    public class MIF : ModSystem
+    {
+        ICoreClientAPI api;
+    }
+    class GetCollisionBoxes_Patch
     {
         static bool Prefix(ref Cuboidf[] __result)
         {
